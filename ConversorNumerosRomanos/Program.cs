ac1381a1d6ac4141357f0d6926e8e5b0662cdc7c
Blaiva <blayverthreyesp14@gmail.com>
2025-09-02 00:37:59 -0400

    Se agrego la funcionalidad de recibir un numero romano por consola y el metodo ConvertirRomanoEntero

--- ConversorNumerosRomanos/Program.cs
+++ ConversorNumerosRomanos/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace ConversorNumerosRomanos
+﻿using System.Globalization;
+
+namespace ConversorNumerosRomanos
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            Console.Write("Introduzca el numero romano a convertir: ");
+            char[] numeroRomano = Console.ReadLine().ToUpper().ToCharArray();
+
+            Console.WriteLine(ConvertirRomanoEntero(numeroRomano));
         }
+
+        public static double ConvertirRomanoEntero(char[] numeroRomano)
+        {
+            double numeroEntero = 0;
+            for(int i = 0; i < numeroRomano.Length; i++)
+            {
+                if(numeroRomano[i].Equals('I'))
+                {
+                    if (i + 1 < numeroRomano.Length && numeroRomano[i+1].Equals('V'))
+                    {
+                        numeroEntero += 4;
+                        i++;
+                        continue;
+                    }
+                    numeroEntero++;
+                }
+            }
+
+            return numeroEntero;
+        }
+
+
     }
 }
